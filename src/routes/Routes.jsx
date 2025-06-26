6dbfce0903fd324b5396419d267dd45bc79b6fda
Likhonpaul2 <likhonpaul2@gmail.com>
2025-06-26 15:11:36 +0600

    change readmefile

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -11,7 +11,7 @@ import MyGroups from "../Pages/MyGroups";
 import UpdateGroup from "../Pages/UpdateGroup";
 import GroupDetails from "../Pages/GroupDetails";
 import PrivateRoutes from "./PrivateRoutes";
-import ErrorPage from "../Pages/ErrorPage";
+import ErrorPage from "../Pages/ErrorPage"; 
 
 export const router = createBrowserRouter([
   {
